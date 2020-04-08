fca46d73b7d8fb4a598bc6d2d25966707ebe0e2c
lyascat <lyascat@gmail.com>
2020-04-08 16:29:06 -0500

    Updates

---
+++ precache-manifest.0d696560eee5f89a28c01cf028e27914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52872c73f0eb5d8cb7579b1612ed95a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4b8028d6a57b444b54",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3fdfdc4ea2fba054dc36",
+    "url": "/static/js/2.b5283797.chunk.js"
+  },
+  {
+    "revision": "04d4ae29da28052f6deb4520be091b23",
+    "url": "/static/js/2.b5283797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4b8028d6a57b444b54",
+    "url": "/static/js/main.6cd32a64.chunk.js"
+  },
+  {
+    "revision": "7110acc990d25f8c3c0f",
+    "url": "/static/js/runtime-main.58e0bcff.js"
+  },
+  {
+    "revision": "c9b05f5180c31e70db89ea7c5c9163ad",
+    "url": "/static/media/logo.c9b05f51.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burger-queen/precache-manifest.bda5042ec891afa6a1f1c2d33d483702.js"
+  "/precache-manifest.0d696560eee5f89a28c01cf028e27914.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger-queen/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
